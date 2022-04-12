a2fe4ba350b17981f3565d3f49b8ba20a3f4302f
Gideon Yeboah Asante <asanteg36@gmail.com>
2022-04-12 23:00:55 +0000

    fix 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,17 @@
 #include "main.h"
 /**
- * main - Funtion to print _putchar
+ * main - Function to print _putchar
  * Return: 0
  */
 int main(void)
 {
 char str[8] = "_putchar";
-for (int letter = 0; letter <= 8; letter++)
+int letter = 0;
+while (letter <= 8)
+{
 _putchar(str[letter]);
+letter++;
+}
 _putchar('\n');
 return (0);
 }
