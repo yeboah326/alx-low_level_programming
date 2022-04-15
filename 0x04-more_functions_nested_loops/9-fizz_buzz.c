c9c7db0726c98f02a75249f991a4d6fab870caa8
Gideon Yeboah Asante <asanteg36@gmail.com>
2022-04-15 20:31:42 +0000

    Fix script 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,23 +16,26 @@ while (num <= 100)
 
 if ((num % 3 == 0) & (num % 5 == 0))
 {
-printf("FizzBuzz ");
+printf("FizzBuzz");
 }
 
 else if (num % 3 == 0)
 {
-printf("Fizz ");
+printf("Fizz");
 }
 
 else if (num % 5 == 0)
 {
-printf("Buzz ");
+printf("Buzz");
 }
 
 else
 {
-printf("%d ", num);
+printf("%d", num);
 }
+
+if (num != 100)
+printf(" ");
 num++;
 }
 }
